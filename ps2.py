ad6549f7120bddab965cf474b69101284abb7f89
sunil1468 <sunil1468@gmail.com>
2017-09-12 15:17:07 +0530

    Update 1

--- ps2.py
+++ ps2.py
@@ -1,3 +1,44 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+
+def drawBoard(board):
+
+  6.     # This function prints out the board that it was passed.
+
+  7.
+
+  8.     # "board" is a list of 10 strings representing the board (ignore index 0)
+
+  9.     print('   |   |')
+
+ 10.     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+ 11.     print('   |   |')
+
+ 12.     print('-----------')
+
+ 13.     print('   |   |')
+
+ 14.     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+
+ 15.     print('   |   |')
+
+ 16.     print('-----------')
+
+ 17.     print('   |   |')
+
+ 18.     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+
+ 19.     print('   |   |')
+
+print("Welcome to the game!")
+print("Player 1's chance")
+
+#playerInput{}
+"""while True:
+	k = raw_input('Please enter the module ID: ')
+	val = raw_input('Please enter the grade for the module: ')
+        grades[k] = val
+
+"""
