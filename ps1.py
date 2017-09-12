f4de323681d4e37678e750fe248f6ea1edb735bf
sunil1468 <sunil1468@gmail.com>
2017-09-12 14:01:47 +0530

    Update 1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,19 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+sample = input("Input Bits :")
+check = 0;
+
+for i in input
+	if(i!="0" || i!="1"):
+		break;
+	else:
+		if i="1":
+			check=check+1
+		else:
+			continue
+if (check/2 == 0)
+	print("sample"+"1")
+else
+	print("sample"+"0")
