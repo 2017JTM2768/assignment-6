d01923d4797bdae0be0319be69308e9137da1634
2017JTM2768 <31790535+2017JTM2768@users.noreply.github.com>
2017-10-12 13:52:45 +0530

    Update 4

--- ps1.py
+++ ps1.py
@@ -1,58 +1,51 @@
+from pip._vendor.distlib.compat import raw_input
+
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
 
 ## Parity Checking
 
-sample = input("Sample Bits :")
+sample = raw_input("Sample Bits :")
 check = 0;
 
-for i in sample
-	if(i!="0" || i!="1"):
-		break;
-	else:
-		if i="1":
-			check=check+1
-		else:
-			continue
-if (check/2 == 0)
-	print("sample"+"1")
-else
-	print("sample"+"0")
+for i in sample:
+    if(i!="0" or i!="1"):
+        break;
+    else:
+        if i=="1":
+            check=check+1
+        else:
+            continue
+if (check/2 == 0):
+    print("sample"+"1")
+else:
+    print("sample"+"0")
 
 ## Bit Stuffing
-temp = None
-count = 0
-output = None 
-for i in sample:
-	temp = temp+i
-	count = count+1
-	if (count==3 and temp = "010"):
-		output = output+temp+'0'
-		temp=None
-		count = 0
-		i=i-2 	#going back two spaces in the sample
-		continue
-	else:
-		continue
-
+temp = str(None)
+output = str(None) 
+for i in range(len(sample)):
+    temp = temp+sample[i]
+    output=output+sample[i]
+    if (len(temp)<3):
+        continue
+    else:
+        if(temp[len(temp)-3]+temp[len(temp)-2]+temp[len(temp)-1]=='010'):
+            output=output+'0'
+        
+    
 #adding 0101 at the end of sample
 
-length = len(sample)
-lasttwo = None
-for j in sample range(length-2,length):
-	lasttwo=lasttwo+j
-	if(lasttwo=="10"):
-		output = output+'00101'	#frame ends in 01, a 0 would be stuffed after the 1st 0 in the actual terminating string 0101.
-	else:
-		output = output+"0101"
+if ((sample[len(sample)-2]+sample[len(sample)-1])=='01'):
+    output=output+'00101'
+else:
+    output=output+'0101'
 
-print('output')
-	
+print(output)
+    
 
 
 
-	
-		
-		
-
+    
+        
